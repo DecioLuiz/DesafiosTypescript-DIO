9badcb472018d013b80b5a72ff2873c162ab5c33
DecioLuiz <decioluiz@msn.com>
2022-06-28 23:45:03 -0300

    Entrega dos 3 desafios de código - TypeScript

---
+++ Desafio01/app.js
@@ -0,0 +1,8 @@
+"use strict";
+let employee = {
+    code: 10,
+    name: 'Jhon'
+};
+employee.code = 10;
+employee.name = "John";
+console.log(employee);
---
+++ Desafio01/app.ts
@@ -0,0 +1,9 @@
+let employee: {code: number, name: string} = {
+    code: 10,
+    name: 'Jhon'
+}
+
+employee.code = 10;
+employee.name = "John";
+
+console.log(employee);
